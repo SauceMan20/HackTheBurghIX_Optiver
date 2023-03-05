1e28a57e9c5d1862946213915d0e1ed94ca3b872
Ismaeel Bashir <93952193+ismaeelbashir03@users.noreply.github.com>
2023-03-05 09:40:11 +0000

    Delete eternal.py

--- optiver_comp/eternal.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-import time
-from optibook.synchronous_client import Exchange
-
-while True:
-    absolute_path = os.getcwd()
-    subprocess.run(["python3", absolute_path + "/esg_trading/main_model.py"])
-    
